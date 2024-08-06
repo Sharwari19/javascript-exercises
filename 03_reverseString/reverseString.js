93e057dcb15fb40b53f23638bcde7239edecb0be
sharwarigole <sharwarigole@gmail.com>
2024-08-06 16:21:42 +0530

    write a function to reverse a string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,19 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    
+    const stringToArr = string.split('');
+    const reverseArray = [];
+    
+    for(let i = stringToArr.length - 1; i >= 0; i--)
+    {
+        reverseArray.push(stringToArr[i]);
+    }
 
+    let arrayToString = reverseArray.join("");
+
+    return arrayToString;
 };
+
+reverseString('hello there');
 
 // Do not edit below this line
 module.exports = reverseString;
