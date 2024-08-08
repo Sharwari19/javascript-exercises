e75fd250e8a0f8b7f2f2bf089ac9c3f59c8bf307
sharwarigole <sharwarigole@gmail.com>
2024-08-08 12:45:09 +0530

    write a function to perform sum of number between(including them) two positive integers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,40 @@
-const sumAll = function() {
+const sumAll = function(startPoint, endPoint) {
 
+    let i = startPoint;
+    let iSum = 0;
+    let isStartInteger =  Number.isInteger(startPoint);
+    let isEndInteger = Number.isInteger(endPoint);
+
+    if( startPoint < 0                    || 
+        endPoint < 0                      || 
+        !(typeof startPoint === "number") ||
+        !(typeof endPoint === "number")   ||
+        !isStartInteger                   ||
+        !isEndInteger 
+    )
+    {
+        return 'ERROR';
+    }
+
+    if(startPoint > endPoint)
+    {
+        for(i = startPoint; i >= endPoint; i--)
+        {
+            iSum = iSum + i;
+        }
+    }
+    else
+    {
+        for(i = startPoint; i <= endPoint; i++)
+        {
+            iSum = iSum + i;
+        }
+    }
+
+    return iSum;
 };
+
+sumAll(1, 4);
 
 // Do not edit below this line
 module.exports = sumAll;
