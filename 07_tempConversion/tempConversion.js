5a115bb835b765d7beaf2708e8d9829c952bbd11
sharwarigole <sharwarigole@gmail.com>
2024-08-08 13:54:24 +0530

    write functions to convert temp to Farenhite and Celcius

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,27 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInF) {
+
+  let tempInC = Math.round((tempInF - 32) * (5/9) * 10)/ 10;
+  return tempInC;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInC) {
+
+  let tempInF = Math.round(((tempInC * 9)/5 + 32) * 10) / 10;
+  return tempInF;
+  
 };
+
+convertToCelsius(32);
+convertToFahrenheit(100);
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
   convertToFahrenheit
 };
+
+
+// x °C ≘ (x × ⁠9/5 + 32) F
+
+// 	x °F ≘ (x − 32) × 5/9 C ⁠
