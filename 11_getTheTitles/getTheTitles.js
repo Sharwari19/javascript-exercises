68661f358ad5abbf198e2715dd111855dd028e54
sharwarigole <sharwarigole@gmail.com>
2024-09-05 16:44:13 +0530

    write a function to get array of object properties from array of objects

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,9 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
 
+    let bookNames = books.map((book) => book.title);
+    return bookNames;
 };
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
